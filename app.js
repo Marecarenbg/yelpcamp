cd5baed7e3c32bea32db517628e240e5541d0e59
Marko Narancic <stajexexe@hotmail.com>
2017-12-14 01:42:35 +0000

    update db url

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ var commentRoutes = require("./routes/comments"),
      mongoose.Promise = global.Promise;
 
 
-// mongoose.connect("mongodb://localhost/yelp_camp_v7");
-mongoose.connect("mongodb://kamperica:123456@ds125565.mlab.com:25565/kamperica");
+mongoose.connect(process.env.DATABASEURL);
+// mongoose.connect("mongodb://kamperica:123456@ds125565.mlab.com:25565/kamperica");
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
